f6e6577a6317fc111f7fdf912cb6e171862a751a
tysonOliveira <thaisonoliveira@gmail.com>
2022-10-10 23:20:36 -0300

    style(global.ts, default.ts, button.styles): configure global styles
    Configura os estilos globais e adiciona no Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import { ButtonContainer, ButtonVariant } from './Button.styles';
 interface ButtonProps {
   variant?: ButtonVariant;
 }
-
+// teste
 export function Button({ variant = 'primary' }: ButtonProps) {
   return <ButtonContainer variant={variant} >Enviar</ButtonContainer>
 }
